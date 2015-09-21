fcabc274fe56d41d93300836acf0ebf7c58b1b9a
ashlynbaum <a@ashlynbaum.com>
2015-09-21 16:57:07 +1200

    [wip] add background canvas

--- js/script.js
+++ js/script.js
@@ -24,41 +24,46 @@ function initHeader() {
     canvas.width = width;
     canvas.height = height;
     ctx = canvas.getContext('2d');
+
+    canvasbg = document.getElementById('canvasbg');
+    canvasbg.width = width;
+    canvasbg.height = height;
+    ctxbg = canvas.getContext('2d');
 }
 
 // min and max radius, radius threshold and percentage of filled circles
-var radMin = 5,
-  radMax = 125,
+var radMin = 30,
+  radMax = 60,
   // filledCircle = 60, //percentage of filled circles
   // concentricCircle = 30, //percentage of concentric circles
   radThreshold = 25; //IFF special, over this radius concentric, otherwise filled
 
 //min and max speed to move
-var speedMin = 0.1,
-  speedMax = 0.3;
+var speedMin = 0.05,
+  speedMax = 0.1;
 
 //max reachable opacity for every circle and blur effect
 var maxOpacity = 0.5;
 
 //default palette choice
-var colors = ['52,168,83', '117,95,147', '199,108,23', '194,62,55', '0,172,212', '120,120,120'];
-//   bgColors = ['52,168,83', '117,95,147', '199,108,23', '194,62,55', '0,172,212', '120,120,120'],
-  // circleBorder = 10,
+// var colors = ['52,168,83', '117,95,147', '199,108,23', '194,62,55', '0,172,212', '120,120,120'],
+//   bgColors = ['52,168,83', '117,95,147', '199,108,23', '194,62,55', '0,172,212', '120,120,120'];
+//   // circleBorder = 10,
   // backgroundLine = bgColors[0];
 var backgroundMlt = 0.85;
 
 //min distance for links
-var linkDist = Math.min(canvas.width, canvas.height) / 2.4,
+var linkDist = Math.min(canvas.width, canvas.height) / 3,
   lineBorder = 2.5;
 
 //most importantly: number of overall circles and arrays containing them
-var maxCircles = 25,
-  points = [];
-  // pointsBack = [];
+var maxCircles = 20,
+  points = [],
+  pointsBack = [];
 
 //populating the screen
 for (var i = 0; i < maxCircles * 2; i++) points.push(new Circle());
-// for (var i = 0; i < maxCircles; i++) pointsBack.push(new Circle(true));
+for (var i = 0; i < maxCircles; i++) pointsBack.push(new Circle(true));
 
 //experimental vars
 var circleExp = 1,
@@ -70,14 +75,14 @@ var circleExp = 1,
 //circle class
 function Circle(background) {
   //if background, it has different rules
-  // this.background = (background || false);
+  this.background = (background || false);
   this.x = randRange(-canvas.width / 2, canvas.width / 2);
   this.y = randRange(-canvas.height / 2, canvas.height / 2);
   this.radius = background ? hyperRange(radMin, radMax) * backgroundMlt : hyperRange(radMin, radMax);
   // this.filled = this.radius < radThreshold ? (randint(0, 100) > filledCircle ? false : 'full') : (randint(0, 100) > concentricCircle ? false : 'concentric');
   // this.color = background ? bgColors[randint(0, bgColors.length - 1)] : colors[randint(0, colors.length - 1)];
   // this.borderColor = background ? bgColors[randint(0, bgColors.length - 1)] : colors[randint(0, colors.length - 1)];
-  this.opacity = 0.05;
+  this.opacity = 0.01;
   this.speed = (background ? randRange(speedMin, speedMax) / backgroundMlt : randRange(speedMin, speedMax)); // * (radMin / this.radius);
   this.speedAngle = Math.random() * 2 * Math.PI;
   this.speedx = Math.cos(this.speedAngle) * this.speed;
@@ -110,26 +115,6 @@ function hyperRange(a, b) {
 
 //rendering function
 function drawCircle(ctx, circle) {
-//   //circle.radius *= circleExp;
-//   var radius = circle.background ? circle.radius *= circleExp : circle.radius /= circleExp;
-//   ctx.beginPath();
-//   ctx.arc(circle.x, circle.y, radius * circleExp, 0, 2 * Math.PI, false);
-//   ctx.lineWidth = Math.max(1, circleBorder * (radMin - circle.radius) / (radMin - radMax));
-//   ctx.strokeStyle = ['rgba(', circle.borderColor, ',', circle.opacity, ')'].join('');
-//   if (circle.filled == 'full') {
-//     ctx.fillStyle = ['rgba(', circle.borderColor, ',', circle.background ? circle.opacity * 0.8 : circle.opacity, ')'].join('');
-//     ctx.fill();
-//     ctx.lineWidth=0;
-//     ctx.strokeStyle = ['rgba(', circle.borderColor, ',', 0, ')'].join('');
-//   }
-//   ctx.stroke();
-//   // if (circle.filled == 'concentric') {
-//   //   ctx.beginPath();
-//   //   ctx.arc(circle.x, circle.y, radius / 2, 0, 2 * Math.PI, false);
-//   //   ctx.lineWidth = Math.max(1, circleBorder * (radMin - circle.radius) / (radMin - radMax));
-//   //   ctx.strokeStyle = ['rgba(', circle.color, ',', circle.opacity, ')'].join('');
-//   //   ctx.stroke();
-//   // }
   circle.x += circle.speedx;
   circle.y += circle.speedy;
   if (circle.opacity < (circle.background ? maxOpacity : 1)) circle.opacity += 0.01;
@@ -144,24 +129,24 @@ function init() {
 
 // //rendering function
 function draw() {
-//   if (circlePulse) {
-//     if (circleExp < circleExpMin || circleExp > circleExpMax) circleExpSp *= -1;
-//     circleExp += circleExpSp;
-//   }
+  // if (circlePulse) {
+  //   if (circleExp < circleExpMin || circleExp > circleExpMax) circleExpSp *= -1;
+  //   circleExp += circleExpSp;
+  // }
   var ctxfr = document.getElementById('canvas').getContext('2d');
-//   // var ctxbg = document.getElementById('canvasbg').getContext('2d');
+  var ctxbg = document.getElementById('canvasbg').getContext('2d');
 
   ctxfr.globalCompositeOperation = 'destination-over';
   ctxfr.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
-//   // ctxbg.globalCompositeOperation = 'destination-over';
-//   // ctxbg.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
+  ctxbg.globalCompositeOperation = 'destination-over';
+  ctxbg.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
 
   ctxfr.save();
   ctxfr.translate(canvas.width / 2, canvas.height / 2);
-//   // ctxbg.save();
-//   // ctxbg.translate(canvas.width / 2, canvas.height / 2);
+  ctxbg.save();
+  ctxbg.translate(canvas.width / 2, canvas.height / 2);
 
-//   //function to render each single circle, its connections and to manage its out of boundaries replacement
+  //function to render each single circle, its connections and to manage its out of boundaries replacement
   function renderPoints(ctx, arr) {
     for (var i = 0; i < arr.length; i++) {
       var circle = arr[i];
@@ -190,7 +175,7 @@ function draw() {
           ctx.moveTo(arr[i].x + xi, arr[i].y + yi);
           ctx.lineTo(arr[j].x + xj, arr[j].y + yj);
           var samecolor = arr[i].color == arr[j].color;
-          ctx.strokeStyle = ["rgba(", arr[i].borderColor, ",", Math.min(arr[i].opacity, arr[j].opacity) * ((linkDist - dist) / linkDist), ")"].join("");
+          ctx.strokeStyle = ["rgba(", arr[i].borderColor, ",", Math.min(arr[i].opacity, arr[j].opacity) * ((linkDist - dist) / linkDist)/10, ")"].join("");
           ctx.lineWidth = (arr[i].background ? lineBorder * backgroundMlt : lineBorder) * ((linkDist - dist) / linkDist); //*((linkDist-dist)/linkDist);
           ctx.stroke();
         }
@@ -200,11 +185,11 @@ function draw() {
 
   var startTime = Date.now();
   renderPoints(ctxfr, points);
-  // renderPoints(ctxbg, pointsBack);
+  renderPoints(ctxbg, pointsBack);
   deltaT = Date.now() - startTime;
 
   ctxfr.restore();
-//   // ctxbg.restore();
+  ctxbg.restore();
 
   window.requestAnimationFrame(draw);
 }
