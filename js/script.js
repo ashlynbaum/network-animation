ff881ddaa9952879bb6a3a7d79c38fd6f8d80ed4
Ashlyn Baum <a@ashlynbaum.com>
2015-09-22 20:57:55 +1200

    Add event listeners.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var width, height, largeHeader, canvas, ctx, circles, target, animateHeader = tr
 
 // Main
 initHeader();
+addListeners();
 
 function initHeader() {
     width = window.innerWidth - 25;
@@ -20,6 +21,42 @@ function initHeader() {
     canvasbg.width = width;
     canvasbg.height = height;
     ctxbg = canvas.getContext('2d');
+}
+
+// Event handling
+function addListeners() {
+    if(!('ontouchstart' in window)) {
+        window.addEventListener('mousemove', mouseMove);
+    }
+    window.addEventListener('scroll', scrollCheck);
+    window.addEventListener('resize', resize);
+}
+
+function mouseMove(e) {
+    var posx = posy = 0;
+    if (e.pageX || e.pageY) {
+        posx = e.pageX;
+        posy = e.pageY;
+    }
+    else if (e.clientX || e.clientY)    {
+        posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+        posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    }
+    target.x = posx;
+    target.y = posy;
+}
+
+function scrollCheck() {
+    if(document.body.scrollTop > height) animateHeader = false;
+    else animateHeader = true;
+}
+
+function resize() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    largeHeader.style.height = height+'px';
+    canvas.width = width;
+    canvas.height = height;
 }
 
 // min and max radius, radius threshold and percentage of filled circles
